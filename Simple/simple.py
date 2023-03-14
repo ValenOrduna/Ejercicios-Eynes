a84b73b810294fbb1a6d0ef327d4233f53157fc2
ValenOrduna <valenordu@hotmail.com>
2023-03-13 22:23:00 -0300

    Ejercicio Simple Completado

--- Simple/simple.py
+++ Simple/simple.py
@@ -9,7 +9,9 @@ def main() -> list:
     for i in range(10):
         # Obtenemos un numero aleaotorio entre 1 y 100 utilizando la funcion randint del modulo random
         age:int =  randomNumber(1,100)
-        # Creamos el diccionario con las llaves id (Se genera un id unico utilizando la funcion uuid4 del modulo uuid) y edad
+        """ Creamos el diccionario con las llaves id 
+        (Se genera un id unico utilizando la funcion uuid4 del modulo uuid) 
+        y edad """
         user = {'id':str(uuid.uuid4()),'edad':age}
         # A continuacion comento otra forma de generar un id
         
@@ -20,5 +22,15 @@ def main() -> list:
 
     # Retornamos los usuarios totales
     return users
+
+# Funcion que ordena la lista de mayor a menor en base a la edad
+def sortList(users) -> list:
+    """ Utilizamos la funcion sorted, como primer parametro le pasamos los usuarios,
+    en el atributo key inicializamos una funcion lambda para obtener el atributo edad
+    y por ultimo el atributo reverse lo instanciamos en True"""
+    result = sorted(users, key=lambda x: x['edad'], reverse=True)
+    
+    # Retonamos la lista ordenada de mayor a menor en base a la edad
+    return result
 
 
